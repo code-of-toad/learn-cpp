db2b49eb50d64042cdf9dafb245e219e08dd5082
Danny <cool.huip@gmail.com>
2024-05-14 01:11:20 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,20 @@
-//
-// Created by danny on 2024-05-13.
-//
+#include <iostream>
+#include <vector>
+#include "student.h"
+
+using std::cout, std::endl, std::string, std::vector;
+
+void cppVectors();
+
+int main() {
+//    cppVectors();
+    return 0;
+}
+
+void cppVectors() {
+    vector<float> nums = {11.2f, 22.1f, 33, 44, 55};
+
+    for (auto val : nums) {
+        cout << val << '\n';
+    } cout << '\n';
+}
