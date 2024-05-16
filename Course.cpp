98c439e90445a315e6b992ef0e86a05b59e955ab
Danny <cool.huip@gmail.com>
2024-05-16 02:43:34 -0400

    Create Course class

---
+++ Course.cpp
@@ -0,0 +1,5 @@
+//
+// Created by danny on 2024-05-16.
+//
+
+#include "Course.h"
---
+++ Course.h
@@ -0,0 +1,14 @@
+//
+// Created by danny on 2024-05-16.
+//
+
+#ifndef LEARN_CPP_COURSE_H
+#define LEARN_CPP_COURSE_H
+
+
+class Course {
+
+};
+
+
+#endif //LEARN_CPP_COURSE_H
