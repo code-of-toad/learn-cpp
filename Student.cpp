c5e5764dca8d007257f311e091d1873170a236ae
Danny <cool.huip@gmail.com>
2024-05-16 02:42:23 -0400

    Update Student class

--- Student.cpp
+++ Student.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
 #include "Student.h"
+
+using std::cout, std::endl;
+
+Student::Student(const string& first,
+                 const string& last,
+                 int id,
+                 float avg)
+    : _first(first)
+    , _last(last)
+    , _id(id)
+    , _avg(avg) {}
+
+int Student::getID() const {
+    return _id;
+}
+
+int Student::getAvg() const {
+    return _avg;
+}
+
+const string& Student::getFirst() const {
+    return _first;
+}
+
+const string& Student::getLast() const {
+    return _last;
+}
--- Student.h
+++ Student.h
@@ -1,10 +1,23 @@
 #ifndef LEARN_CPP_STUDENT_H
 #define LEARN_CPP_STUDENT_H
 
+#include <string>
+
+using std::string;
 
 class Student {
-
+    string _first = "First";
+    string _last  = "Last";
+    int    _id    = 0;
+    float  _avg   = 0;
+public:
+    Student() = default;
+    Student(const string& first, const string& last, int id, float avg);
+    int getID() const;
+    int getAvg() const;
+    const string& getFirst() const;
+    const string& getLast() const;
+    void print() const;
 };
-
 
 #endif //LEARN_CPP_STUDENT_H
