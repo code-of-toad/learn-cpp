0069a0a4a8d524434ca68fe3400422544ea6d1e4
Danny <cool.huip@gmail.com>
2024-05-16 03:40:44 -0400

    Finish coding client code (for practice)

--- Main.cpp
+++ Main.cpp
@@ -8,11 +8,17 @@ int main() {
     Student s2("Dave", "Churchill", 1, 3.14);
     Student s3("Bruce", "Lee", 7359, 3.99);
 
-    Course c;
-    c.addStudent(s1); c.addStudent(s2); c.addStudent(s3);
+    Course c("COMP3400");
+    c.addStudent(s1);
+    c.addStudent(s2);
+    c.addStudent(s3);
 
     c.loadFromFile("students.txt");
-    c.print();
+    c.printStudents();
+
+//    for (const Student& s : c.getStudents()) {
+//        s.print();
+//    }
 
     return 0;
 }
